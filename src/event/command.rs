3d0cb7ebe5ca2791c05b4fe63163367cec286d70
Daniel Gallagher <pdanielgallagher@gmail.com>
2018-06-26 21:07:06 -0700

    Adds ACI L2CAP Connection Parameter Update Request return parameters

--- src/event/command.rs
+++ src/event/command.rs
@@ -13,12 +13,38 @@ use core::convert::TryInto;
 /// Complete](hci::event::Event::CommandComplete::Vendor) event. If the commands have defined return
 /// parameters, they are included in the enum.
 #[derive(Clone, Debug)]
-pub enum ReturnParameters {}
+pub enum ReturnParameters {
+    /// Status returned by the [ACI L2CAP Connection Parameter Update
+    /// Request](::ActiveBlueNRG::aci_l2cap_connection_parameter_update_request) command.
+    AciL2CapConnectionParameterUpdateRequest(hci::Status),
+}
 
 impl hci::event::VendorReturnParameters for ReturnParameters {
     type Error = super::BlueNRGError;
 
-    fn new(_buffer: &[u8]) -> Result<Self, Self::Error> {
-        Err(super::Error::UnknownEvent(0))
+    fn new(bytes: &[u8]) -> Result<Self, hci::event::Error<Self::Error>> {
+        check_len_at_least(bytes, 3)?;
+
+        match hci::Opcode(LittleEndian::read_u16(&bytes[1..])) {
+            ::opcode::L2CAP_CONN_PARAM_UPDATE_REQ => Ok(
+                ReturnParameters::AciL2CapConnectionParameterUpdateRequest(to_status(&bytes[3..])?),
+            ),
+            other => Err(hci::event::Error::UnknownOpcode(other)),
+        }
     }
+}
+
+fn check_len_at_least(
+    buffer: &[u8],
+    len: usize,
+) -> Result<(), hci::event::Error<super::BlueNRGError>> {
+    if buffer.len() < len {
+        Err(hci::event::Error::BadLength(buffer.len(), len))
+    } else {
+        Ok(())
+    }
+}
+
+fn to_status(bytes: &[u8]) -> Result<hci::Status, hci::event::Error<super::BlueNRGError>> {
+    bytes[0].try_into().map_err(hci::event::rewrap_bad_status)
 }
---
+++ tests/command_complete.rs
@@ -0,0 +1,44 @@
+extern crate bluenrg;
+extern crate bluetooth_hci as hci;
+
+use bluenrg::event::command::ReturnParameters as BNRGParams;
+use bluenrg::event::*;
+use hci::event::command::ReturnParameters as HciParams;
+use hci::event::{Event as HciEvent, Packet};
+
+type Event = HciEvent<BlueNRGEvent>;
+
+macro_rules! status_only {
+    {
+        $($(#[$inner:ident $($args:tt)*])*
+        $fn:ident($oc0:expr, $oc1:expr, $return:path);)*
+    } => {
+        $(
+            $(#[$inner $($args)*])*
+            #[test]
+            fn $fn() {
+                let buffer = [0x0E, 4, 8, $oc0, $oc1, 0];
+                match Event::new(Packet(&buffer)) {
+                    Ok(HciEvent::CommandComplete(event)) => {
+                        assert_eq!(event.num_hci_command_packets, 8);
+                        match event.return_params {
+                            HciParams::Vendor($return(status)) => {
+                                assert_eq!(status, hci::Status::Success);
+                            }
+                            other => panic!("Wrong return parameters: {:?}", other),
+                        }
+                    }
+                    other => panic!("Did not get command complete event: {:?}", other),
+                }
+            }
+        )*
+    }
+}
+
+status_only! {
+    aci_l2cap_connection_parameter_update_request(
+        0x81,
+        0xFD,
+        BNRGParams::AciL2CapConnectionParameterUpdateRequest
+    );
+}
